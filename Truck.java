bf93699786ffc08be2d3f406b3b8c26c28cd5129
DGOPU <divyasrirani.gopu@gmail.com>
2020-11-03 00:03:25 +0530

    Modified

--- Truck.java
+++ Truck.java
@@ -1,16 +1,16 @@
 package GITProjects;
 
-abstract class Vehicle1 {
+abstract class Vehicle {
     private char vehicleType;
     public abstract void move ();
 }
 
-public class Truck1 extends Vehicle1 {
+public class Truck extends Vehicle {
     private int truckNo;
 
 
     public static void main(String args[]) {
-        Truck1 mahindra= new Truck1();
+        Truck mahindra= new Truck();
         mahindra.move();
     }
 
